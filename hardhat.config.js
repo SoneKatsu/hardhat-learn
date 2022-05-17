4f8595f90eaaaf5b5451d20c99093296c2886284
SoneKatsu <seungli000201@gmail.com>
2022-05-17 21:56:58 +0900

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      chainId: 4,
+      accounts: [`e00d0345785cf06213698e6936c344973f6937cf902c5a508490e8ed612a96b7`]
+    }
+  }
+};
